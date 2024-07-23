28206d6360578c2a256b1e5d7d5564fe4642e691
Fenslau <c01@bk.ru>
2024-07-23 17:16:20 +0700

    Product Policy

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,6 @@ Route::post('login', [AuthController::class, 'login'])
 Route::apiResources([
     'products' => ProductController::class,
 ]);
+Route::post('products', [ProductController::class, 'store'])->can('create', Product::class);
+Route::put('products/{product}', [ProductController::class, 'update'])->can('update', 'product');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->can('delete', 'product');
